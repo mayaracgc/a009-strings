665dfd8ac99b6bcfc2801d1c8fcf7cf76d7396d0
mayaracgc <mayaracgcgmail.com>
2022-08-31 15:42:41 -0300

    exercicio feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString)
+console.log("Quantos caracteres tem a frase? " + minhaString.length)
+console.log("E agora, quantos tem? " + minhaString.trim().length)
+const minhaNovaString = minhaString.replace("________" , "sticioso")
+console.log(minhaNovaString)
+
+
